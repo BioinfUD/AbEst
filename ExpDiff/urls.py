4f2a0aa47c71bed1201ca23344fc2502a04203f5
nazkter <nazkter@nazkter.nazkter>
2014-09-12 02:09:08 -0500

    file upload

--- ExpDiff/urls.py
+++ ExpDiff/urls.py
@@ -1,19 +1,21 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('processing.views',
-	url(r'^upload_file/$', 'upload_file', name="upload_file"),
     url(r'^$', 'home', name='home'),
-    url(r'^upload_success/$', 'upload_success', name='upload success'),
     url(r'^bowtie/$', 'bowtie_form'),
     url(r'^bwa/$', 'bwa_form'),
     url(r'^diffexp/$', 'diffexp_form'),
     url(r'^register/$', 'register_user'),
     url(r'^login/$', 'log_in'),
     url(r'^login/auth/$', 'auth_view'),
+    url(r'^logout/$', 'log_out'),
+    url(r'^files/$', 'show_files'),
+    url(r'^files/upload/$', 'show_fileupload'),
+    url(r'^files/submit/$', 'filesubmit', name='filesubmit'),
+    url(r'^files/success/$', 'upload_success', name='upload_success'),
     # Examples:
     # url(r'^$', 'ExpDiff.views.home', name='home'),
     # url(r'^ExpDiff/', include('ExpDiff.foo.urls')),
@@ -22,5 +24,5 @@ urlpatterns = patterns('processing.views',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
