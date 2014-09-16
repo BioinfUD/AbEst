89ccd63f08058e4b53d05cff2887738dd2aaf6e8
alejo0317 <alejandro.0317@gmail.com>
2014-09-16 17:25:09 -0500

    Bowtie mapping added

--- processing/models.py
+++ processing/models.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+
 from django.db import models
 import subprocess
 import threading
 from django.conf import settings
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.models import User
+from time import sleep
+from django.core.files import File as Django_File
+from django.conf import settings
+
+
+
 
 #OPciones estáticas
 POSIBLES_ESTADOS_PROCESOS = (
@@ -17,16 +25,19 @@ MAPEADORES = (
         (0, "BWA"),
         (1, "Bowtie"),
     )
+TIPOS_MAPEO = (
+        (0, "Single end"),
+        (1, "Paired end")
+    )
 # Create your models here.
 
 
 class Proceso(models.Model):
 
-    estado = models.IntegerField(choices=POSIBLES_ESTADOS_PROCESOS, default="3") 
+    estado = models.IntegerField(choices=POSIBLES_ESTADOS_PROCESOS, default=3) 
     std_err = models.TextField(default="")
     std_out = models.TextField(default="")
     comando = models.CharField(max_length = 2000, default="echo Hola mundo")
-
 
     class Meta:
         verbose_name_plural = 'Procesos'
@@ -36,7 +47,7 @@ class Proceso(models.Model):
         self.estado = 2
         self.save()
         try:
-            p = subprocess.Popen(self.comando.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p = subprocess.Popen(str(self.comando).split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             self.std_out, self.std_err = p.communicate()
             self.estado = p.returncode
         except:
@@ -59,24 +70,6 @@ class Proceso(models.Model):
     def __unicode__(self):
         return u"ID: %s Estado: %s \n Comando: %s \n STDOUT: \n %s \n STDERR: %s\n " % (str(self.id), str(self.estado), str(self.comando), str(self.std_out), str(self.std_err))
 
-
-class Mapeo(models.Model):
-    in_left = []
-    in_right = []
-
-    proceso =  models.ForeignKey(Proceso)
-    mapeador = models.IntegerField(choices=MAPEADORES) 
-
-    class  Meta:
-        verbose_name_plural = "Procesos de mapeo"
-
-
-
-class ExpDiff(models.Model):
-    sam_input = [ ]
-
-         
-
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User)
@@ -87,14 +80,83 @@ class Profile(models.Model):
     def __unicode__(self):
         return unicode(self.email)
 
-'''formulario para subir foto de perfil
-class ImageUploadForm(forms.Form):
-    image = forms.ImageField()
-'''
-
 class File(models.Model): 
     fileUpload = models.FileField() 
-    description = models.TextField() 
+    description = models.TextField(default="") 
     profile = models.ForeignKey(Profile) # formulario para subir cualquier archivo 
     def __unicode__(self):
         return unicode(self.description)
+
+class Mapeo(models.Model):
+    name = models.TextField(default="Experimento")
+    procesos =  models.ManyToManyField(Proceso)
+    mapeador = models.IntegerField(choices=MAPEADORES, default=1) 
+    tipo = models.IntegerField(default=1, choices=TIPOS_MAPEO)
+    profile = models.ForeignKey(Profile) 
+    out_file = models.ForeignKey(File, null=True)
+
+    def run_bowtie(self, reference, reads_1="", reads_2="", reads_se="",type=1):
+        reference_file = reference #Full path
+        reference_path = reference_file[::-1].split(" ",2)[1][::-1]
+        reference_index = reference_path + "/" + reference_file.split("/")[-1].replace(" ","_").replace(".fasta", "").replace(".fa", "")
+        comando = "bowtie-build %s %s" % (reference_index, reference_file)
+        p1 = Proceso(comando=comando)
+        p1.save()
+        self.procesos.add(p1)
+        #To get files with path trin.fileUpload.path
+        #Genero indice y espero hasta que este listo
+        t1 = threading.Thread(target=p1.run_process)
+        t1.setDaemon(True)
+        t1.start()
+        while t1.isAlive():
+            sleep(1)
+        file_name = "/tmp/%s.bam" % self.name.replace(" ", "_") 
+        if self.tipo==1:
+            mates1 = "','.join(reads_1)"
+            mates2 = "','.join(reads_2)"
+            #out_bam = "%s.bam" % self.name.replace(" ", "_")
+            comando = "bowtie -p %s  %s -1 %s -2 %s  - > %s | samtools view -bSh - > %s " % (settings.CORES, reference_index, mates1, mates2, file_name)
+        elif self.tipo==0:
+            mates=","join(reads_se)
+            comando = "bowtie -p %s  %s -s %s   - > %s | samtools view -bSh - > %s " % (settings.CORES, reference_index, mates, file_name)
+        p2 = Proceso(comando=str(comando))
+        p2.save()
+        self.procesos.add(p2)
+        t2 = threading.Thread(target = p2.run_process)
+        t2.setDaemon(True)
+        t2.start()
+        while t2.isAlive():
+            sleep(1)
+        self.save()
+        out_file = File(fileUpload=Django_File(open(file_name)), description="Salida "+ self.name, profile=self.profile)
+        out_file.save()
+        self.out_file = out_file
+
+
+    def run(self):
+        self.name = "Experimento %s" % self.id
+        self.save()
+        if self.mapeador==1:
+            t = threading.Thread(target=self.run_bowtie)
+        elif self.mapeador==2:
+            t = threading.Thread(target=self.run_bowtie) # Replace for bwa when ready
+        t.setDaemon(True)
+        t.run()
+
+
+
+    class  Meta:
+        verbose_name_plural = "Procesos de mapeo"
+
+
+
+class ExpDiff(models.Model):
+    sam_input = [ ]
+
+
+"""
+
+formulario para subir foto de perfil
+class ImageUploadForm(forms.Form):
+    image = forms.ImageField()
+"""
